ac6957253b120ba23fa11205dd1dbff0287249a4
unknown <khomanesuraj8@gmail.com>
2021-12-02 20:46:02 +0530

    validate input name

--- js/employeePayrollForm.js
+++ js/employeePayrollForm.js
@@ -4,4 +4,13 @@ output.textContent = salary.value;
 salary.addEventListener('input', function () {
     output.textContent = salary.value;
 });
+// checking for name validation
+const text = document.querySelector('#name');
+const textError = document.querySelector('.text-error');
+text.addEventListener('input', function() {
+    let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+    if (nameRegex.test(text.value))
+        textError.textContent = "";
+    else textError.textContent = "Name is Incorrect";
+});
     
