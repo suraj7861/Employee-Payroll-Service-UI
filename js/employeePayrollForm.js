dc31fec4527da83922a17e26e0b74af4d8a29b29
unknown <khomanesuraj8@gmail.com>
2021-12-03 06:52:51 +0530

    Added event listener when document is loaded

--- js/employeePayrollForm.js
+++ js/employeePayrollForm.js
@@ -1,16 +1,19 @@
-const salary = document.querySelector('#salary');
-const output = document.querySelector('.salary-output');
-output.textContent = salary.value;
-salary.addEventListener('input', function () {
+window.addEventListener('DOMContentLoaded', (event) => {
+    //changing values of salary according to slider
+
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
     output.textContent = salary.value;
+    salary.addEventListener('input', function() {
+        output.textContent = salary.value;
+    });
+    // checking for name validation
+    const text = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    text.addEventListener('input', function() {
+        let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+        if (nameRegex.test(text.value))
+            textError.textContent = "";
+        else textError.textContent = "Name is Incorrect";
+    });
 });
-// checking for name validation
-const text = document.querySelector('#name');
-const textError = document.querySelector('.text-error');
-text.addEventListener('input', function() {
-    let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
-    if (nameRegex.test(text.value))
-        textError.textContent = "";
-    else textError.textContent = "Name is Incorrect";
-});
-    
